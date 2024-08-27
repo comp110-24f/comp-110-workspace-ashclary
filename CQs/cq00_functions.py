0ee6ecb8687de215813bd856f1ce0c7a16a6c1f9
Ashley Clary <ashclary@unc.edu>
2024-08-27 13:27:01 -0400

    challenge question 1

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,12 @@
+"""My first challenge question"""
+
+__author__ = "730754494"
+
+
+def mimic(message: str) -> str:
+    """A function to return a message back"""
+    return message
+
+
+if __name__ == "__main__":
+    print(mimic(message=input("What is your message?")))
--- welcome.py
+++ welcome.py
@@ -5,3 +5,10 @@ __author__ = "01234567"
 print("Welcome to COMP110!")
 print("You are in for a fun adventure into programming!")
 print("<3 the COMP110 Team!")
+
+
+def sum(num1: int, num2: int) -> int:
+    return num1 + num2
+
+
+sum(num1=11, num2=3)
