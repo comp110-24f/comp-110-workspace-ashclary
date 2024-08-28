bd6102cb48c057570fb0de4fae3a080b0486aaf1
Ashley Clary <ashclary@unc.edu>
2024-08-28 14:58:39 -0400

    my first challenge question!

--- CQs/cq00_functions.py
+++ CQs/cq00_functions.py
@@ -1,12 +1,17 @@
-"""My first challenge question"""
+"""My first challenge question!"""
 
 __author__ = "730754494"
 
 
 def mimic(message: str) -> str:
-    """A function to return a message back"""
+    """A function to return a message back."""
     return message
 
 
-if __name__ == "__main__":
+def main() -> None:
+    """A function to print the result of calling mimic."""
     print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
